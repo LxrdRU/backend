f8811c8d895189f7d6bbe2b6cafb135eaa420674
LxrdRU <a981203@mail.ru>
2021-06-02 15:13:12 -0700

    database change

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const appKey = new Clarifai.App({
 const db = require('knex')({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-adjacent-30654',
     user : 'postgres',
     password : 'a981203A',
     database : 'crecognition'
